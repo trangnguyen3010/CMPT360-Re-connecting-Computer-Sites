ef1b0572474e4e17d9d646aff7ac914a308813c7
Trang Nguyen <trang.nguyen@benevity.com>
2023-09-16 23:56:13 -0600

    updated test script

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 for i in {1..11}
 do
+  echo "Check test $i:"
   java -cp "./out/" "main.Main" < "./src/test/in/t$i.in" > "./out/test/t$i.result"
-  diff "./src/test/out/t$i.out" "./out/test/t$i.result"
+  diff "./src/test/out/t$i.out" "./out/test/t$i.result" | tr -d "\\" | tr -d " No newline at end of file"
+  echo "---------------------------------"
 done
