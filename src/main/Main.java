5c7883a22ca4e0908ec3ae6d12b64239475e4369
Trang Nguyen <trang.nguyen@benevity.com>
2023-09-17 16:18:07 -0600

    Passed all tests

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,9 @@ import java.util.OptionalDouble;
 import java.util.OptionalInt;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -48,10 +50,9 @@ public class Main {
         final int k = scanner.nextInt();
         final WeightedGraphAdjListRep280<Vertex280> newGraph = (WeightedGraphAdjListRep280<Vertex280>) oldSpanningTree
                 .clone();
-        newGraph.ensureVertices(n);
 
         for (int i = 0; i < k; i++) {
-        
+
             WeightedEdge280<Vertex280> addedEdge = addEdgeForGraphFromStandarIn(newGraph, scanner);
 
             // Add the edge to the K's edge list
@@ -62,33 +63,34 @@ public class Main {
                 .sorted(Comparator.comparingDouble(WeightedEdge280::getWeight))
                 .collect(Collectors.toList());
 
-        final Optional<Integer> updatedCost = calculateNewCost(newGraph);
+        final double updatedCost = calculateCost(newGraph);
 
         System.out.println(originalCost);
-        System.out.print(updatedCost.isPresent() ? updatedCost.get().intValue() : originalCost);
+        System.out.print((int) updatedCost);
     }
 
-    private static WeightedEdge280<Vertex280> addEdgeForGraphFromStandarIn (final WeightedGraphAdjListRep280<Vertex280> graph, final Scanner scanner){
-            // Get source vertex.
-            int srcIdx = getNextInt(scanner);
+    private static WeightedEdge280<Vertex280> addEdgeForGraphFromStandarIn(
+            final WeightedGraphAdjListRep280<Vertex280> graph, final Scanner scanner) {
+        // Get source vertex.
+        int srcIdx = getNextInt(scanner);
 
-            // Get destination vertex.
-            int dstIdx = getNextInt(scanner);
+        // Get destination vertex.
+        int dstIdx = getNextInt(scanner);
 
-            // Get the weight of the edge.
-            int weight = getNextInt(scanner);
+        // Get the weight of the edge.
+        int weight = getNextInt(scanner);
 
-            if (graph.isAdjacent(srcIdx, dstIdx))
-                throw new RuntimeException(
-                        "Edge (" + srcIdx + ", " + dstIdx + ") appears multiple times in the data file.");
+        if (graph.isAdjacent(srcIdx, dstIdx))
+            throw new RuntimeException(
+                    "Edge (" + srcIdx + ", " + dstIdx + ") appears multiple times in the data file.");
 
-            // Add the new edge.
-            graph.addEdge(srcIdx, dstIdx);
+        // Add the new edge.
+        graph.addEdge(srcIdx, dstIdx);
 
-            // Set the weight of the new edge.
-            graph.setEdgeWeight(srcIdx, dstIdx, weight);
+        // Set the weight of the new edge.
+        graph.setEdgeWeight(srcIdx, dstIdx, weight);
 
-            return graph.getEdge(srcIdx, dstIdx);
+        return graph.getEdge(srcIdx, dstIdx);
     }
 
     private static int getNextInt(final Scanner scanner) {
@@ -98,48 +100,52 @@ public class Main {
         return number;
     }
 
-    private static Optional<Integer> calculateNewCost(WeightedGraphAdjListRep280<Vertex280> newGraph) {
-        if (newGraph.numEdges() == 0) {
-            return Optional.empty();
-        } else {
-            ArrayList<Vertex280> isSeen = new ArrayList<>();
-            double sum = 0.0;
-            for (int i = 1; i <= newGraph.numVertices(); i++) {
-                if (!isSeen.contains(newGraph.vertex(i))) {
-                    isSeen.add(newGraph.vertex(1));
-                    sum += calculateCost(newGraph, newGraph.vertex(1), isSeen);
-                }
-            }
-            return Optional.of((int) sum);
+    private static double calculateCost(WeightedGraphAdjListRep280<Vertex280> newGraph) {
+        final Vertex280 firstVertex = newGraph.vertex(1);
+        Set<Vertex280> isSeen = new HashSet<>();
+
+        isSeen.add(firstVertex);
+        List<WeightedEdge280<Vertex280>> edgeOptions = newGraph.getAdjacentVertices(firstVertex);
+
+        edgeOptions = edgeOptions.stream()
+                .filter(e -> !isSeen.contains(e.other(firstVertex)))
+                .sorted(Comparator.comparingDouble(WeightedEdge280::getWeight))
+                .collect(Collectors.toList());
+
+        double sum = 0.0;
+        while (!edgeOptions.isEmpty() && isSeen.size() != newGraph.numVertices()) {
+            // the least-heavy edge
+            WeightedEdge280<Vertex280> choosenEdge = edgeOptions.get(0);
+
+            Vertex280 currentVertex = isSeen.contains(choosenEdge.firstItem()) ? choosenEdge.firstItem()
+                    : choosenEdge.secondItem();
+
+            final Vertex280 otherVertex = choosenEdge.other(currentVertex);
+            isSeen.add(otherVertex);
+
+            sum += choosenEdge.getWeight();
+
+            // System.out.println("Choose edge: " + choosenEdge.toString() + " " + choosenEdge.getWeight());
+            // System.out.println("Current vertex: " + currentVertex.toString());
+
+            edgeOptions.remove(0);
+
+            // Add new options
+            List<WeightedEdge280<Vertex280>> newOptions = newGraph.getAdjacentVertices(otherVertex).stream()
+                    .filter(e -> !isSeen.contains(e.other(otherVertex)))
+                    .collect(Collectors.toList());
+
+            edgeOptions.addAll(newOptions);
+
+            edgeOptions = edgeOptions.stream()
+                    .filter(e -> !isSeen.contains(e.firstItem()) || !isSeen.contains(e.secondItem()))
+                    .sorted(Comparator.comparingDouble(WeightedEdge280::getWeight))
+                    .collect(Collectors.toList());
+
+            // edgeOptions.forEach(e -> System.out.println(e.toString()));
+            // isSeen.forEach(e -> System.out.println(e));
         }
-    }
-
-    private static double calculateCost(WeightedGraphAdjListRep280<Vertex280> newGraph, Vertex280 currentVertex,
-            ArrayList<Vertex280> isSeen) {
-        if (isSeen.size() == newGraph.numVertices()) {
-            return 0.0;
-        }
-
-        // Get all edges that has current vertex as one end
-        List<WeightedEdge280<Vertex280>> allAdjEdge = new ArrayList<>();
-        allAdjEdge.addAll(newGraph.getAdjacentVertices(currentVertex));
-
-        // Find the least-heavy edge that incident at a vertex which are not seen yet
-        Optional<WeightedEdge280<Vertex280>> nextEdge = allAdjEdge.stream().sorted(Comparator.comparingDouble(WeightedEdge280::getWeight))
-                .filter(e -> !isSeen.contains(e.other(currentVertex)))
-                .findFirst();
-
-        // this current vertex is a leaf node in the spaning tree
-        if (!nextEdge.isPresent()) {
-            return 0.0;
-        } // not a leaf node, continue spanning
-        else {
-            isSeen.add(nextEdge.get().other(currentVertex));
-            allAdjEdge.remove(nextEdge);
-            double sum = nextEdge.get().getWeight()
-                    + calculateCost(newGraph, nextEdge.get().other(currentVertex), isSeen);
-            return sum;
-        }
+        return sum;
     }
 
 }
