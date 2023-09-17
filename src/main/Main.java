ff8f7fece9aa95226858b219d4a183072010521f
Trang Nguyen <trang.nguyen@benevity.com>
2023-09-16 18:20:29 -0600

    compiled the necessary packages in lib280 to use in our main package

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,19 @@
 package main;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Trang is here!");
+        //Read inputs
+        final Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println("Input: " + line);
+        }
+
+        final int originalCost = -1;
+        final int updatedCost = -1;
+        System.out.println(originalCost);
+        System.out.println(updatedCost);
     }
 }
